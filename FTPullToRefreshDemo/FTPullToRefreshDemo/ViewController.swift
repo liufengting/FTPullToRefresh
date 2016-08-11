5598cce90f14d4f6349b79500ecceef4d22dcb05
liufengting <wo157121900@me.com>
2016-08-11 13:44:31 +0800

    edit

---
+++ FTPullToRefreshDemo/FTPullToRefreshDemo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  FTPullToRefreshDemo
+//
+//  Created by liufengting on 16/8/11.
+//  Copyright © 2016年 liufengting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        self.tableView.addPullRefreshHeaderWithRefreshBlock(0);
+        
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView .dequeueReusableCellWithIdentifier("FTPullToRefreshCellIdentifier", forIndexPath: indexPath)
+        
+        
+        return cell
+    }
+
+}
+
